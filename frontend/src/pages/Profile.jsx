72b08463b09b2995a4ea2e421e4d7fe2974502fc
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-31 15:42:31 +0200

    change name in components

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileEcole from "../components/ProfileEcole";
+import ProfileEntreprise from "../components/ProfileEntreprise";
+
+function Profilepage() {
+  return (
+    <div>
+      <ProfileEcole />
+      <ProfileEntreprise />
+    </div>
+  );
+}
+
+export default Profilepage;
