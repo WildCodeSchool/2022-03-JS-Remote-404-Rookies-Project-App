421c898bccb47d2f1d8a6b6fd40b7dc76d82d59f
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-05-31 16:37:37 +0200

    navbar created and  updated with css + pathing

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,12 @@
+import Navbar from "@components/Navbar";
 import React from "react";
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Profile;
