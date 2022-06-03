72c9a7aa727bd132d0807785e37f9b187da2b524
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-06-03 10:22:31 +0200

    fixing troubles

--- frontend/src/pages/Profilepage.jsx
+++ frontend/src/pages/Profilepage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import ProfileEcole from "../components/Profileécole";
+import ProfileEcole from "../components/ProfileEcole";
 import ProfileEntreprise from "../components/Profileentreprise";
+import Navbar from "../components/Navbar";
+
+import users from "../assets/dataset/users.json";
 
 function Profilepage() {
+  const user = users[1]; // users 1 =   ecole dans la  bdd test  users 2 =   entreprise dans la  bdd test
+
   return (
-    <div>
-      <ProfileEcole />
-      <ProfileEntreprise />
+    <div className="flex">
+      <Navbar />
+      <div className="flex-col w-full">
+        {user.entity_category_id === 1 && <ProfileEcole />}
+        {user.entity_category_id === 2 && <ProfileEntreprise />}
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -2,41 +2,10 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 
-import NewProjectTypeCompany from "../components/NewProjectTypeCompany";
-import NewProjectDescriptionCompany from "../components/NewProjectDescriptionCompany";
-import NewProjectDomainCompany from "../components/NewProjectDomainCompany";
-import NewProjectConsultingCompany from "../components/NewProjectConsultingCompany";
-import SchoolProgressStep from "../components/SchoolProgressStep";
-import CompanyProgressStep from "../components/CompanyProgressStep";
-
-
-// import NewProjectOrganisationSchool from "../components/NewProjectOrganisationSchool";
-// import NewProjectDescriptionSchool from "../components/NewProjectDescriptionSchool";
-// import NewProjectDomainSchool from "../components/NewProjectDomainSchool";
-// import NewProjectDatesClésSchool from "../components/NewProjectDatesClésSchool";
-// import NewProjectProfileEcoleSchool from "../components/NewProjectProfileEcoleSchool";
-
 function Project() {
   return (
-
-<div className="flex">
-<Navbar />
-<div className="flex-col  w-full ">
-<CompanyProgressStep CompanyProgressStep={CompanyProgressStep} />
-        <SchoolProgressStep SchoolProgressStep={SchoolProgressStep} />
-      </div>
-
-	<NewProjectTypeCompany />
-      <NewProjectDescriptionCompany />
-      <NewProjectDomainCompany />
-      <NewProjectConsultingCompany />
-	{/*<NewProjectOrganisationSchool /> */}
-      {/* <NewProjectDescriptionSchool /> */}
-      {/* <NewProjectDomainSchool /> */}
-      {/* <NewProjectDatesClésSchool /> */}
-      {/*<NewProjectProfileEcoleSchool />*/}
-
-
+    <div className="flex h-full">
+      <Navbar />
     </div>
   );
 }
