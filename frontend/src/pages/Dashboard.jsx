12523aba0234ce66ad723c1b1ddc52270ebbd49c
Lionel Manzetti <lionel.manzetti@gmail.com>
2022-06-03 10:43:22 +0200

    Basic design for the dashboard, adapted the dataset according to the needs and basic components

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,23 @@
-import Navbar from "@components/Navbar";
 import React from "react";
 
+import Navbar from "../components/Navbar";
+import User from "../components/User";
+import Entity from "../components/Entity";
+import MyProjects from "../components/MyProjects";
+
+import users from "../assets/dataset/users.json";
+
 function Dashboard() {
+  const user = users[2];
+
   return (
-    <div>
+    <div className="flex">
       <Navbar />
+      <div className="flex flex-col w-full">
+        <User user={user} />
+        <Entity user={user} />
+        <MyProjects />
+      </div>
     </div>
   );
 }
