7894e3e1e93173800d60bb9d450293fe45fec75f
Saucy Corentin <saucy.corentin@gmail.com>
2022-06-09 14:27:49 +0200

    Finalisation du front de la page project

---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import settings1 from "../assets/settings1.png"
+import avatar1 from "../assets/avatar1.png"
+
+function User() {
+  return (
+    <div className="flex items-center">
+    <button>
+      <img className="mr-5" src={settings1} />
+    </button>
+    <p className="mr-5 text-blue-900 font-semibold">Albert Rookies</p>
+    <img className="img2" src={avatar1} />
+  </div>
+  )
+}
+
+export default User;
--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -1,10 +1,20 @@
 import Navbar from "@components/Navbar";
 import React from "react";
 
+import User from "@components/User";
+import ProjectDetails from "@components/ProjectDetails";
+
 function Project() {
   return (
-    <div>
+    <div className="flex">
       <Navbar />
+      <div className="flex flex-col">
+        <div className="flex justify-between m-10 items-center text-gray-500">
+          <p>Entreprise / Mes Projets / Étude de marché</p>
+          <User />
+        </div>
+        <ProjectDetails />
+      </div>
     </div>
   );
 }
