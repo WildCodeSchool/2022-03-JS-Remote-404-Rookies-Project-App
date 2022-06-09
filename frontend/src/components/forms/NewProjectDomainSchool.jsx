523a0409b03e26c76fad3ea4393848c3a4475abf
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-06-09 11:09:51 +0200

    Add dataset to languages, status and student level and change filds in project forms concerned

--- frontend/src/components/forms/NewProjectDomainSchool.jsx
+++ frontend/src/components/forms/NewProjectDomainSchool.jsx
@@ -35,7 +35,7 @@ export default function NewProjectDomainSchool() {
         </div>
         <div />
       </div>
-      {/* MANQUE DONNEES */}
+      {/* A MAPPER SUR DATASET LANGUAGES */}
       <div>
         <h2 className="text-lg font-semibold">
           Quelles sont les langues disponibles ? *
@@ -45,9 +45,13 @@ export default function NewProjectDomainSchool() {
         </p>
         <div className="inline-block relative m-5 w-full bg-white border border-black rounded-md">
           <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-            <option>Langue 1</option>
-            <option>Langue 2</option>
-            <option>Langue 3</option>
+            <option>Français</option>
+            <option>Anglais</option>
+            <option>Allemand</option>
+            <option>Espagnol</option>
+            <option>Arabe</option>
+            <option>Russe</option>
+            <option>Portuguais</option>
           </select>
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg
--- frontend/src/components/forms/NewProjectOrganisationSchool.jsx
+++ frontend/src/components/forms/NewProjectOrganisationSchool.jsx
@@ -74,14 +74,17 @@ function NewProjectOrganisationSchool() {
             />
           </div>
         </div>
-        {/* MANQUE LES DONNEES */}
+        {/* A MAPPER SUR DATASET NIVEAU ETUDES */}
         <div className="flex flex-col ml-30">
           <h2 className="text-lg font-semibold">Niveau des étudiants ? *</h2>
           <div className="inline-block relative w-96 m-5 bg-white border border-black rounded-md">
             <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-              <option>Niveau 1</option>
-              <option>Niveau 2</option>
-              <option>Niveau 3</option>
+              <option>Bac (CAP, BEP)</option>
+              <option>Bac +2 (BTS, DUT, DEUST)</option>
+              <option>Bac +3 (License, BUT)</option>
+              <option>Bac +4 (Master 1, Maitrise)</option>
+              <option>Bac +5 (Master 2, DEA, DESS, Diplôme ingénieur)</option>
+              <option>Bac +8 (Doctorat)</option>
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg
@@ -95,7 +98,7 @@ function NewProjectOrganisationSchool() {
           </div>
         </div>
       </div>
-      {/* MANQUE LES DONNEES */}
+      {/* A MAPPER SUR VILLES DE FRANCE */}
       <h2 className="text-lg font-semibold">Campus concerné ? *</h2>
       <div className="inline-block relative w-96 m-5 bg-white border border-black rounded-md">
         <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
