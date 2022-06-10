d01bed8c6c21d8917061a2a7cb6173d019517319
Saucy Corentin <saucy.corentin@gmail.com>
2022-06-10 10:33:43 +0200

    Le composant UserSettings JSX est fonctionnel avec la data JSON.

--- frontend/src/components/Dashboard/UserSettings.jsx
+++ frontend/src/components/Dashboard/UserSettings.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import settings1 from "../../assets/pictures/settings1.png"
-import avatar1 from "../../assets/pictures/avatar1.png"
+import randompic from "../../assets/pictures/blank-profile-picture.png";
+
+import users from "../../assets/dataset/users.json";
+
 
 function UserSettings() {
+  const user = users[2];
+
+  const navigate = useNavigate();
+
   return (
     <div className="flex items-center">
-    <button>
+    <button onClick={() => navigate("/dashboard")}>
       <img className="mr-5" src={settings1} />
     </button>
-    <p className="mr-5 text-blue-900 font-semibold">Albert Rookies</p>
-    <img className="img2" src={avatar1} />
+    <p className="mr-5 text-blue-900 font-semibold">{user.firstname} {user.lastname}</p>
+    <img className="img2" src={user.photo ? user.photo : randompic} />
   </div>
   )
 }
