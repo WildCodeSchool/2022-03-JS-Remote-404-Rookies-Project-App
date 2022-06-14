a8d7f06b63f60cc36d3cdd5f0dd40b9d8dfb9cdf
Saucy Corentin <saucy.corentin@gmail.com>
2022-06-14 16:06:35 +0200

    Ajout des images

---
+++ frontend/src/components/Dashboard/UserSettings.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import settings1 from "../../assets/pictures/settings1.png";
+import randompic from "../../assets/pictures/blank-profile-picture.png";
+
+import users from "../../assets/dataset/users.json";
+
+function UserSettings() {
+  const user = users[2];
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center">
+      <button type="submit" onClick={() => navigate("/dashboard")}>
+        <img className="mr-5" src={settings1} alt={settings1} />
+      </button>
+      <p className="mr-5 text-blue-900 font-semibold">
+        {user.firstname} {user.lastname}
+      </p>
+      <img
+        className="img2"
+        src={user.photo ? user.photo : randompic}
+        alt={settings1}
+      />
+    </div>
+  );
+}
+
+export default UserSettings;
--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
+import plusLogo from "../assets/pictures/add.png";
+
+import "../styles/Project.css";
+
+import UserSettings from "../components/Dashboard/UserSettings";
+import DashboardButton from "../components/Dashboard/DashboardButton";
+import ProjectSelection from "../components/Dashboard/ProjectSelection";
+
 function Project() {
   return (
-    <div className="flex h-full">
-      <h1>Projets</h1>
+    <div className="w-full">
+      <div className="flex flex-col">
+        <div className="flex justify-between m-10 items-center text-gray-500">
+          <p>Entreprise / Mes Projets / Étude de marché</p>
+          <UserSettings />
+        </div>
+        <ProjectSelection />
+        <DashboardButton
+          action={{ logo: plusLogo, text: "Ajouter", link: "/submission" }}
+        />
+      </div>
     </div>
   );
 }
