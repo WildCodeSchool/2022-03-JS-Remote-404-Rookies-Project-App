2701daaf7969e93be4f16594aa459963b6453fcf
Saucy Corentin <saucy.corentin@gmail.com>
2022-06-20 17:05:47 +0200

    Ajout de l'URL fetch back-end relié à la base de donnée et passage de la data des matchs via la BDD

--- frontend/src/components/Dashboard/MatchesInProject.jsx
+++ frontend/src/components/Dashboard/MatchesInProject.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-import School from "../../assets/pictures/school1.png";
+import axios from "axios";
+
+import Default from "../../assets/pictures/blank-profile-picture.png";
 
 function MatchesInProject() {
+  const [school, setSchool] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/schools/1`)
+      .then((res) => setSchool(res.data))
+      .catch((err) => console.warn(err));
+  }, []);
+
   return (
     <div className="blocproject4">
       <h1 className="matches">Mes matches :</h1>
       <div className="bloc5">
         <div className="imgschool">
-          <img src={School} alt={School} />
+          <img src={school.image_url ? school.image_url : {Default} } alt={Default} />
         </div>
         <div className="listed">
-          <p className="school">Ecole Group</p>
-          <p className="schoolbis">Patrick Ecole</p>
+          <p className="school">{school.campuses}</p>
+          <p className="schoolbis">{school.website}</p>
         </div>
-        <h1 className="schoolid">
-          Management International - Master Commerce International
-        </h1>
+        <h1 className="schoolid">{school.name}</h1>
         <button type="submit" className="edit">
           Voir plus
         </button>
